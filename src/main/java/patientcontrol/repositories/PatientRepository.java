425ddb91a2b82d94ce426583d0e7c35575413e6f
Denilson87 <denilsonmucavel@outlook.com>
2022-04-21 17:07:21 +0200

    building repository for Patient Model

--- src/main/java/patientcontrol/repositories/PatientRepository.java
+++ src/main/java/patientcontrol/repositories/PatientRepository.java
@@ -1,5 +1,11 @@
 package patientcontrol.repositories;
 
-public interface PatientRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.patientcontrol.models.PatientModel;
+
+public interface PatientRepository extends JpaRepository<PatientModel, UUID>{
 
 }
