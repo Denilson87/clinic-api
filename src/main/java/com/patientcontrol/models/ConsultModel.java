0052f2c5e11bad7c7e19c60825962521e9785ee4
Denilson87 <denilsonmucavel@outlook.com>
2022-04-21 23:43:36 +0200

    adding Relations OneToMany

--- src/main/java/com/patientcontrol/models/ConsultModel.java
+++ src/main/java/com/patientcontrol/models/ConsultModel.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -28,6 +29,71 @@ public class ConsultModel implements Serializable{
 	  private String DateofConsult;
 	  @Column (nullable=false, length=50)
 	  private LocalDateTime registrationdate;
+	  
+	  @OneToMany
+	  private PatientModel patient;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getConsultType() {
+		return ConsultType;
+	}
+
+	public void setConsultType(String consultType) {
+		ConsultType = consultType;
+	}
+
+	public String getConsultName() {
+		return ConsultName;
+	}
+
+	public void setConsultName(String consultName) {
+		ConsultName = consultName;
+	}
+
+	public String getDoctorName() {
+		return DoctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		DoctorName = doctorName;
+	}
+
+	public String getDateofConsult() {
+		return DateofConsult;
+	}
+
+	public void setDateofConsult(String dateofConsult) {
+		DateofConsult = dateofConsult;
+	}
+
+	public LocalDateTime getRegistrationdate() {
+		return registrationdate;
+	}
+
+	public void setRegistrationdate(LocalDateTime registrationdate) {
+		this.registrationdate = registrationdate;
+	}
+
+	public PatientModel getPatient() {
+		return patient;
+	}
+
+	public void setPatient(PatientModel patient) {
+		this.patient = patient;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	  
+	  
   
 	  
 }
